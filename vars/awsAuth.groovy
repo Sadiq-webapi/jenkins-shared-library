8e2db4ede7e1e2c48f392900219f5bcfa083c01d
sadiq <sadiq@9741>
2026-04-14 00:17:45 +0530

    added new files

--- vars/awsAuth.groovy
+++ vars/awsAuth.groovy
@@ -1,9 +1,8 @@
 def call(String awsCreds, String clusterName, String region) {
     echo "Authenticating for cluster: ${clusterName}"
-    withCredentials([aws(credentialsId: awsCreds, 
-                        accessKeyVariable: 'AWS_ACCESS_KEY_ID', 
-                        secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
-        // Run the command directly, do NOT call awsAuth() here
+    withCredentials([usernamePassword(credentialsId: awsCreds, 
+                                     usernameVariable: 'AWS_ACCESS_KEY_ID', 
+                                     passwordVariable: 'AWS_SECRET_ACCESS_KEY')]) {
         sh "aws eks update-kubeconfig --region ${region} --name ${clusterName}"
     }
 }
