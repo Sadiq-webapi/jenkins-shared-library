842b8926c514123a8baf213c0caa7e72f63e2de3
sadiq <sadiq@9741>
2026-04-14 00:27:33 +0530

    added new file

--- vars/awsAuth.groovy
+++ vars/awsAuth.groovy
@@ -4,5 +4,6 @@ def call(String awsCreds, String clusterName, String region) {
                                      usernameVariable: 'AWS_ACCESS_KEY_ID', 
                                      passwordVariable: 'AWS_SECRET_ACCESS_KEY')]) {
         sh "aws eks update-kubeconfig --region ${region} --name ${clusterName}"
+        sh "docker build -t ${dockerUser}/${imageName}:latest ${dockerfileDir}"
     }
 }
